f57a54dc2856f6e76a5bc5f1945a7c710adb535e
Joaquin Cagide <jcagide@ar0c02dt4w0ml85.ml.com>
2021-02-22 13:20:02 -0300

    Ejercicio4

--- src/ejercicios/ejercicio1.java
+++ src/ejercicios/ejercicio1.java
@@ -2,7 +2,7 @@ package ejercicios;
 
 public class ejercicio1 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
     }
 }
--- src/ejercicios/ejercicio4.java
+++ src/ejercicios/ejercicio4.java
@@ -1,8 +1,41 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class ejercicio4 {
 
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Ingrese la cantidad de primeros números primos que desea ver:");
+
+        int n = scanner.nextInt();
+
+        getNPrimes(n);
+
+        scanner.close();
+    }
+
+    public static void getNPrimes(int n)
+    {
+        int cont = 0;
+        int aux = 0;
+
+        while(cont<n){
+            if(isPrime(aux)){
+                cont++;
+                System.out.println(aux);
+            }
+            aux++;
+        }
+    }
+
+    public static boolean isPrime(int n){
+        for(int i=2; i<n; i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
     }
 }
