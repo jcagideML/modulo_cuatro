1e2628c9f917530a2425949c25234f6252c74606
julibarra <julieta.ibarra@mercadolibre.com>
2021-02-22 13:06:56 -0300

    ejercicio 1

--- src/ejercicios/ejercicio1.java
+++ src/ejercicios/ejercicio1.java
@@ -3,6 +3,22 @@ package ejercicios;
 public class ejercicio1 {
 
     public static void main(String[] args){
+        public static void main(String[] args){
+        Scanner in= new Scanner( System.in);
+        System.out.println("Ingrese un numero");
+        int numbre=in.nextInt();
+        for(int i=1; i<= numbre ;i++){
+            if (esPar(i)){
+                System.out.println(i);
+
+            }
+
+        }
+
+    }
+    static boolean esPar(int n){
+        return n%2==0;
+    }
 
     }
 }
