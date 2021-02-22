99b04aa25d1bde3cf5c0a3dae6ff0c8836fec16d
Matias Adriel Heimann <mheimann@ar0c02dt4z9ml85.ml.com>
2021-02-22 13:37:28 -0300

    ej5

--- src/ejercicios/ejercicio5.java
+++ src/ejercicios/ejercicio5.java
@@ -1,8 +1,41 @@
 package ejercicios;
 
-public class ejercicio5 {
+import java.util.Scanner;
 
-    public static void main(String[] args){
+public class ejercico5 {
+
+    public static int getDigitsAppearences(int number, int digit) {
+        int timesAppearing = 0;
+        while(number > 0) {
+            timesAppearing += (number % 10 == digit) ? 1 : 0;
+            number /= 10;
+        }
+        return timesAppearing;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int m = s.nextInt();
+        int d = s.nextInt();
+
+        int i = 0;
+        int j = 1;
+
+        if(m == 1 && d == 0) {
+            System.out.println(0);
+            i++;
+        }
+
+        while(i < n) {
+            if(getDigitsAppearences(j, d) >= m){
+                i++;
+                System.out.println(j);
+            }
+            j++;
+        }
 
     }
 }
+
